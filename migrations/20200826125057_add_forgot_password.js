3f3d5ed4df70f2706296cda4def3289545303daf
Rick Nunes <sanunes.ricardo@gmail.com>
2020-08-27 19:10:39 -0400

    fix: deleted unnecessary field reset_token_created_at

--- migrations/20200826125057_add_forgot_password.js
+++ migrations/20200826125057_add_forgot_password.js
@@ -3,8 +3,7 @@ exports.up = function(knex) {
  return knex.schema.createTable("forgot_password", function (table) {
    table.increments("id");
    table.integer("user_id");
-   table.string("reset_token_digest");
-   table.datetime("reset_token_created_at");
+   table.string("reset_token");
    table.timestamps();
  });
 };
