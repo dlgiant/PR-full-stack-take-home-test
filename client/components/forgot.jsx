afb87812ea6c6d1bb09e495624a91f7061d4ede9
Rick Nunes <sanunes.ricardo@gmail.com>
2020-08-27 19:54:30 -0400

    add: forgot password client component

--- client/components/forgot.jsx
+++ client/components/forgot.jsx
@@ -1,47 +1,62 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { Component } from "react";
+import { notify } from "react-notify-toast";
+import { API_URL } from "../config";
+import { v4 as uuidv4 } from "uuid";
 
-class ForgotForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {email: ''};
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+export default class Forgot extends Component {
+  state = {
+    sendingEmail: false
   }
 
-  handleChange(event) {
-    this.setState({email: event.target.value});
-    console.log("EVENT: ", event.target.email);
-    console.log("EMAIL: ", this.state.email);
-
-  }
-
-  handleSubmit(event) {
-    alert('An email was submitted: '+this.state.email)
-    if (process.env.NODE_ENV == 'production' ){
-      console.log("Submitted email with reset token to: ", this.state.email);
-    } else {
-      console.log("Development: Send email to ", this.state.email, " using stub");
-    }
+  onSubmit = event => {
     event.preventDefault();
+    this.setState({ sendingEmail: true })
+
+    fetch(`${API_URL}/email`, {
+      method: 'post',
+      headers: {
+        accept: 'applications/json',
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({ 
+        email: this.email.value,
+        token: uuidv4()
+      })
+    })
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ sendingEmail: false });
+        notify.show(data.msg);
+        this.form.reset();
+      })
+      .catch(err => console.log(err));
   }
 
-  render() {
+  render = () => {
+    const { sendingEmail } = this.state;
+
     return (
-    <div>
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Email
-          <input name="email" type="email" value={this.state.email} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
+      <form 
+        onSubmit={this.onSubmit}
+        ref={form => this.form = form }
+        >
+        <div>
+          <input
+            type="email"
+            ref={input => this.email = input}
+            required
+          />
+          <label htmlFor='email'> email</label>
+        </div>
+        <div>
+          <button type='submit' className='btn' disabled={sendingEmail}>
+            {sendingEmail
+              ? "Sending reset link"
+              : "Send a reset link"
+            }
+          </button>
+        </div>
       </form>
-    </div>
-    );
+    )
   }
-
 }
-
-export const Forgot = (props) => (
-  <ForgotForm />
-  );
