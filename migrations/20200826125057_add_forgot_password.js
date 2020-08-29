ad9da19ab67413782ae1a4854cdc52079ff66cd3
Rick Nunes <sanunes.ricardo@gmail.com>
2020-08-29 14:12:38 -0400

    migration: added fields to reset pass logic

--- migrations/20200826125057_add_forgot_password.js
+++ migrations/20200826125057_add_forgot_password.js
@@ -1,9 +1,12 @@
 
 exports.up = function(knex) {
  return knex.schema.createTable("forgot_password", function (table) {
-   table.increments("id");
+   table.increments("id")
    table.integer("user_id");
-   table.string("reset_token");
+   table.string("token_digest");
+   table.integer("count_checks"); // Increases every time client start forgot password process
+   table.boolean("setting_password"); // Set to true only after user clicks reset link
+   table.datetime("expires"); 
    table.timestamps();
  });
 };
