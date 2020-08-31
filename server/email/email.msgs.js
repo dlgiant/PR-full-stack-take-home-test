38c5ac75939fd288ca301a98dab7a1736808352e
Rick Nunes <sanunes.ricardo@gmail.com>
2020-08-31 11:50:18 -0400

    fix: renamed files to service forgot password

--- server/email/email.msgs.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    confirm: "Email sent, please check inbox",
-    confirmed: "Email confirmed",
-    resend: "Confirmation email resent, check spam",
-    couldNotFind: "Could not find your email!",
-    alreadyConfirmed: "Your email was already confirmed!",
-    passwordReset: "Your password was successfully reset"
-}
